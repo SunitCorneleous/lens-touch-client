8a987a101fbc064de2a43ad81d6ba02b8e002d3c
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-11-07 22:32:39 +0600

    files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl md:text-6xl bg-blue-600 text-white py-5 text-center font-bold cursor-pointer hover:bg-blue-500">
+        Lens Touch
+      </h1>
     </div>
   );
 }
