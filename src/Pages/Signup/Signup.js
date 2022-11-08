b81b7c951ff6d6eef7f6acfa5057ce3ddba91b5c
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-11-08 21:09:05 +0600

    signup ui added

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 
-const Login = () => {
+const Signup = () => {
   const submitHandler = event => {
     event.preventDefault();
   };
@@ -10,9 +10,34 @@ const Login = () => {
   return (
     <div className="w-11/12 md:w-10/12 mt-16 mb-10 mx-auto">
       <div className="p-5 border border-red-300 rounded  w-11/12 md:w-2/6  mx-auto">
-        <h1 className="text-center text-3xl font-bold">Log In Now</h1>
+        <h1 className="text-center text-3xl font-bold">Sign Up Now</h1>
 
         <form onSubmit={submitHandler} className="mt-5">
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Enter your name:</span>
+            </label>
+            <input
+              required
+              type="text"
+              name="name"
+              placeholder="Type your name"
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">
+                Enter your profile picture link:
+              </span>
+            </label>
+            <input
+              type="text"
+              name="picture"
+              placeholder="Profile picture link"
+              className="input input-bordered w-full"
+            />
+          </div>
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Enter your email:</span>
@@ -37,9 +62,9 @@ const Login = () => {
               className="input input-bordered w-full"
             />
             <p className="text-sm mt-3">
-              <span>Don't have an account?</span>{" "}
-              <Link to="/signup" className="hover:text-red-600 hover:underline">
-                Sign Up
+              <span>Already have an account?</span>{" "}
+              <Link to="/login" className="hover:text-red-600 hover:underline">
+                Log In
               </Link>
             </p>
           </div>
@@ -55,11 +80,11 @@ const Login = () => {
 
         <button className="border border-red-600 text-red-600 hover:bg-red-600 hover:text-white ease-in duration-300 font-semibold py-2 px-3 rounded block mx-auto mt-5">
           <FaGoogle className="inline-block mr-2 mb-1"></FaGoogle>
-          Continue with Google
+          Sign up with Google
         </button>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Signup;
